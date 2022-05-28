261540fad13131006d38140221d4660c28f72af2
Kyle Bondo <gagglepod@gmail.om>
2022-05-28 10:56:44 -0500

    Project Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuZrk6Wp_fxvEm8VI6jwlXLNwp06vlaZw",
+  authDomain: "chaff-live-chat.firebaseapp.com",
+  projectId: "chaff-live-chat",
+  storageBucket: "chaff-live-chat.appspot.com",
+  messagingSenderId: "650744645615",
+  appId: "1:650744645615:web:75e9024c955d3f2e58d81a",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firebase.Fieldvalue.serverTimestamp;
+
+export { projectFirestore, timestamp };
