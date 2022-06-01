c04fa05c09088d81cd8631fb4d9bd32b54548463
Kyle Bondo <gagglepod@gmail.om>
2022-06-01 14:52:41 -0500

    Auto-scroll to bottom

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,23 +1,43 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <!-- <div v-if="documents" class="messages"> -->
-      <div v-for="doc in documents" :key="doc.id" class="single">
-        <span class="created-at">{{ doc.createdAt.toDate() }}</span>
+    <div v-if="documents" class="messages" ref="messages">
+      <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
+        <span class="created-at">{{ doc.createdAt }}</span>
         <span class="name">{{ doc.name }}</span>
         <span class="message">{{ doc.message }}</span>
       </div>
-    <!-- </div> -->
+    </div>
   </div>
 </template>
 
 <script>
 import getCollection from '../composables/getCollection'
+import { formatDistanceToNow } from 'date-fns'
+import { computed, onUpdated, ref } from 'vue'
 
 export default {
   setup() {
     const { error, documents } = getCollection('messages')
-    return { error, documents }
+
+    const formattedDocuments = computed(() => {
+        if (documents.value) {
+            return documents.value.map(doc => {
+                let time = formatDistanceToNow(doc.createdAt.toDate())
+                return { ...doc, createdAt: time }
+            })
+        }
+    })
+
+    // auto-scroll to bottom of messages
+    const messages = ref(null)
+
+    onUpdated(() => {
+        messages.value.scrollTop = messages.value.scrollHeight
+    })
+
+
+    return { error, documents, formattedDocuments, messages }
   }
 }
 </script>
